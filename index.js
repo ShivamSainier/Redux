48c4dd8896ddd3b5def761c271cd672527b3ce58
shivam saini <59383963+shivam007-ss@users.noreply.github.com>
2020-12-26 18:03:10 +0530

    Async Action

--- index.js
+++ index.js
@@ -1,119 +1,44 @@
 const redux=require("redux")
-const createStrore=redux.createStore;
-const combineReducers=redux.combineReducers;
-const applyMiddleware=redux.applyMiddleware;
+const createStore=redux.createStore;
 
-const inital_state={
-    apple:20,
-    mango:30,
-    guava:4,
-    kivi:5}
 
-const inital_state_1={
-    guava:4,
-    kivi:5
+const State={
+    loading:false,
+    user:[],
+    error:''
 }
 
-const apple=()=>{
-    return{
-        type:"apple",
-        info:"my first state"
-    }
-}
-const mango=()=>{
-    return{
-        type:"mango",
-        ino:"i wanna buy mango"
-
-    }
-}
-
-const guava=()=>{
-    return{
-        type:"guava",
-        info:"i wanna buy guava"
-    }
-}
-
-const kivi=()=>{
+const User_Request=()=>{
     return {
-        type:"kivi",
-        info:"i wanna buy kivi sir"
+        type:"User_request",
+        payload:"Request"
     }
 }
 
-const Reducer_minus=(state=inital_state,action)=>{
-    switch (action.type) {
-        case "apple":
-            return{
-                ...state,
-                apple:state.apple-1}
-        case "mango":
-            return{
-                ...state,
-                mango:state.mango-1
-            }
-            case "guava":
-            return{...state,guava:state.guava-1}
-        
-        case "kivi":
-            return{...state,kivi:state.kivi-1}
-        
-        default:return state
-    }}
-
-
-const Reducer_plus=(state=inital_state,action)=>{
-    switch (action.type) {
-        case "apple":
-        return{
-        ...state,
-        apple:state.apple+1}
-        case "mango":
-             return{
-        ...state,
-        mango:state.mango+1
+const User_Success=()=>{
+    return {
+        type:"User_Success",
+        payload:"Success"
     }
-        case "guava":
-            return{...state,guava:state.guava+1}
-        
-        case "kivi":
-            return{...state,kivi:state.kivi+1}
-        
-        default:return state
+}
+
+const User_Error=()=>{
+    return {
+        type:"User_Error",
+        payload:"Error"
     }
+}
+
+const Reducer=(state=State,action)=>{
+    switch (action.type) {
+        case 'User_Request':return{...state,loading:true}
+            
+        case 'User_Success':return{loading:false,user:action.payload,error:''}
+
+        case 'User_Error' : return {loading:false,user:[],error:payload}
     
-}
-
-const Reducer=combineReducers({
-    plus:Reducer_plus,
-    minus:Reducer_minus
-
-})
-
-const logger=store=>{
-    return next=>{
-        return action=>{
-            const result=next(action)
-            console.log("middleWare :",result)
-            return result
-        }
+        default:
+            break;
     }
 }
 
-const Store=createStrore(Reducer,applyMiddleware(logger))
-console.log("initial State",Store.getState())
-const unsubscribe=Store.subscribe(()=>{
-console.log("Updated Value",Store.getState())})
-Store.dispatch(apple())
-Store.dispatch(mango())
-Store.dispatch(apple())
-Store.dispatch(guava())
-Store.dispatch(apple())
-Store.dispatch(apple())
-Store.dispatch(kivi())
-Store.dispatch(apple())
-Store.dispatch(kivi())
-Store.dispatch(mango())
-
-unsubscribe()
